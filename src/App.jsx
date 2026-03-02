75cde5e16019c45e790eafc287447c3dcd664906
abhaypanchalprogrammer <abhaypanchal2525@gmail.com>
2026-03-03 00:39:06 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import Register from "./Auth/Register.jsx";
-import Login from "./Auth/Login.jsx";
+
 import Wrapper from "./shared/Wrapper.jsx";
-import Container from "./shared/container.jsx";
 
 const App = () => {
   return (
